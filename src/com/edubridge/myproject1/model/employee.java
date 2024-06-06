f1f4998c25c971b0afe496f1c667a126a21d8f6c
basha7799 <sk.galibasha@gmail.com>
2024-06-06 11:23:26 +0530

    employee is updated

--- src/com/edubridge/myproject1/model/employee.java
+++ src/com/edubridge/myproject1/model/employee.java
@@ -4,5 +4,23 @@ public class employee {
      private int employeeId;
      private String employeename;
      private long employeeMobile;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeename() {
+		return employeename;
+	}
+	public void setEmployeename(String employeename) {
+		this.employeename = employeename;
+	}
+	public long getEmployeeMobile() {
+		return employeeMobile;
+	}
+	public void setEmployeeMobile(long employeeMobile) {
+		this.employeeMobile = employeeMobile;
+	}
      
 }
